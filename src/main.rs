09b634e96fe8e9545b17c1ea3a74b536a9df0075
ritalat <ritalat@fastmail.com>
2023-04-03 20:45:57 +0300

    Move camera to its own struct

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ use std::io;
 use std::io::prelude::*;
 use std::io::BufWriter;
 
+use crate::camera::Camera;
 use crate::color::write_color;
 use crate::hittable::Hittable;
 use crate::ray::Ray;
 use crate::sphere::Sphere;
-use crate::vec3::{Color, Point, Vec3};
+use crate::vec3::{Color, Point};
 
+mod camera;
 mod color;
 mod hittable;
 mod ray;
@@ -50,16 +52,7 @@ fn main() -> io::Result<()> {
         100.0,
     )));
 
-    // Camera
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = Point::from(0.0, 0.0, 0.0);
-    let horizontal = Vec3::from(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::from(0.0, viewport_height, 0.0);
-    let lower_left_corner =
-        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::from(0.0, 0.0, focal_length);
+    let camera = Camera::new();
 
     // Render
     let f = File::create("img.ppm")?;
@@ -73,10 +66,7 @@ fn main() -> io::Result<()> {
         for i in 0..image_width {
             let u = i as f64 / (image_width - 1) as f64;
             let v = j as f64 / (image_height - 1) as f64;
-            let r = Ray::from(
-                origin,
-                lower_left_corner + u * horizontal + v * vertical - origin,
-            );
+            let r = camera.get_ray(u, v);
             let color = ray_color(&r, &mut objects);
             write_color(&mut writer, &color)?;
         }
